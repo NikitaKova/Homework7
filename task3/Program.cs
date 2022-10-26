58cb67ee72a983fdd93959625bcf403e66e218a5
NikitaKovalev <nik.kovalev.00@bk.ru>
2022-10-26 21:31:37 +0300

    Task3

---
+++ task3/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] CreateArray(int lenRows, int lenColumns)
+{
+    int[,] array = new int[lenRows, lenColumns];
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(0, 10); 
+        }
+    }
+    return array;
+}
+
+int[,] MiddleNum(int[,] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i , j];
+        }
+        Console.WriteLine($"Среднее арифметическое элементов столбца {i + 1} = {sum / 3}");
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t"); 
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] array = CreateArray(3, 4);
+PrintArray(array);
+MiddleNum(array);
